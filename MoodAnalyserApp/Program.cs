2f0fd3b947d72d98fe03cbe802a74886ad381ee5
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-10 09:37:23 +0530

    [NIJAM] Add:Welcome To MoodAnalyser Testing.

---
+++ MoodAnalyserApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***MOOD_ANALYSER***");
+        }
+    }
+}
---
+++ MoodAnalyserTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyserApp;
+namespace MoodAnalyserTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly Program program;
+        public UnitTest1()
+        {
+            program = new Program();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

b9fbf19d03bddf9cdd1de6109ca86cfeae7455fd
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-12-10 15:35:12 +0530

    [NIJAM]Add:Use Reflection to create mood adnalyser

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -6,66 +6,50 @@ namespace programTest
     public class UnitTest1
     {
         /// <summary>
-        /// This Test case ensure AnalyseMood() method will return when SAD when message contains Sad
+        /// This Test case ensure AnalyseMood() 
         /// </summary>
         [TestMethod]
         public void GivenSadMessage_WhenSad_ShouldReturnSAD()
         {
-            //Arrange
             string Expected = "SAD";
             Program program = new Program("I am in Sad Mood");
-            //Act
             string result = program.AnalyseMood();
-            //Assert
             Assert.AreEqual(Expected, result);
         }
-
         /// <summary>
-        /// This Test case ensure AnalyseMood() method will return when HAPPY when message contains Happy
+        ///  method will return when HAPPY when message contains Happy
         /// </summary>
         [TestMethod]
         public void GivenHappyMessage_WhenAnyMood_ShouldReturnHAPPY()
         {
-            //Arrange
             string Expected = "HAPPY";
             Program program = new Program("I am in Happy Mood");
-            //Act
             string result = program.AnalyseMood();
-            //Assert
             Assert.AreEqual(Expected, result);
         }
-
         /// <summary>
-        /// This Test case ensure AnalyseMood() method will return when HAPPY when Any mood message is given
+        /// method will return when HAPPY when Any mood message is given
         /// </summary>
         [TestMethod]
         public void GivenAnyMessage_WhenAnyMood_ShouldReturnHAPPY()
         {
-            //Arrange
             string Expected = "HAPPY";
             Program program = new Program("I am in Any Mood");
-            //Act
             string result = program.AnalyseMood();
-            //Assert
             Assert.AreEqual(Expected, result);
         }
-
         /// <summary>
-        /// This Test case ensure AnalyseMood() method will return when HAPPY when Null mood message is given
+        /// method will return when HAPPY when Null mood message is given
         /// </summary>
         [TestMethod]
         [DataRow("null")]
         public void GivenNULLMessage_WhenANULL_ShouldReturnHAPPY(string message)
         {
-            //Arrange
             string Expected = "Happy";
             Program moodAnalyser = new Program(message);
-            //Act
             string result = moodAnalyser.AnalyseMood();
-            //Assert
             Assert.AreEqual(Expected, result);
         }
-
         /// <summary>
         /// This Test case ensure when given empty message will throw an exception
         /// </summary>
@@ -75,11 +59,8 @@ namespace programTest
         {
             try
             {
-                //Arrange
                 Program program = new Program(message);
-                //Act
                 string result = program.AnalyseMood();
-                //Assert
             }
             catch (MoodAnalyserException e)
             {
@@ -93,7 +74,14 @@ namespace programTest
         public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyserObject()
         {
             object expected = new Program();
-            object obj = MoodAnalyserFactory.CreateeMoodAnalyse("MoodAnalyserProject.MoodAnalyser", "MoodAnalyser");
+            object obj = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyserApp.Program", "Program");
+            expected.Equals(obj);
+        }
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyserObject_UsingParameterizdConstructor()
+        {
+            object expected = new Program("HAPPY");
+            object obj = MoodAnalyserFactory.CreateMoodAnalyserUsingParameterizedConstructor("MoodAnalyserApp.Program", "Program", "HAPPY");
             expected.Equals(obj);
         }
     }
